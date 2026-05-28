4704c416d3e7738fb02c2a366192ca3e4ce4c29c
vajramanikevala <vajramanikevala@gmail.com>
2026-05-28 17:19:18 +0000

    Scripting files added

---
+++ Linux/Scripting/script1.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+echo "Hello Mani"
+
+echo "Wellcome to Bash Scripting"
+
+pwd
+ls
+whoami
+hostname
+date
---
+++ Linux/Scripting/script2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+echo "My Automation Script"
+
+date
+whoami
+hostname
+
+
